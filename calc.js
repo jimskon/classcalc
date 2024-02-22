a0d2e844ba7f1e23d7a8952dc4c5d912303a70a4
James Skön <skonjp@kenyon.edu>
2024-02-22 15:42:01 -0500

    Update calc.js

--- calc.js
+++ calc.js
@@ -5,7 +5,7 @@ var buttons = document.querySelectorAll("button");
 for (var button of buttons) {
   button.addEventListener("click", function (e) {
     var value = e.target.getAttribute("data-value");
-    if (value !in ["=","C"]) {      
+    if !(value in ["=","C"]) {      
       result.innerHTML += value;
     }
   });
