ed8ca28b5c5d95eb4b6ceef7f5e16fd33f83de89
James Skön <skonjp@kenyon.edu>
2024-02-22 15:55:06 -0500

    Update calc.js

--- calc.js
+++ calc.js
@@ -2,9 +2,19 @@
 var result = document.getElementById("result");
 var buttons = document.querySelectorAll("button");
 
+function isOp(v) {
+  if (v == "X" || v == "/" || v == "+" || v == "-")
+    return true;
+  else
+    return false;
+}
+
 for (var button of buttons) {
   button.addEventListener("click", function (e) {
     var value = e.target.getAttribute("data-value");
+    if (isOp(value) && isOp(result.innerHTML[-1]) {
+      return;
+    }
     if (value == "X") {
       value = "*";
     }
